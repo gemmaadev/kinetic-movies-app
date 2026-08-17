1a506399b80b297b51d5baa8104ef47c144a4b02
Gemma Maeso <gemmaa.dev@gmail.com>
2026-08-17 16:30:27 +0200

    feat(app): wire up ErrorBoundary around RouterProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router/dom";
 import { router } from "./app/router";
+import ErrorBoundary from "./app/providers/ErrorBoundary";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ErrorBoundary>
+      <RouterProvider router={router} />
+    </ErrorBoundary>
   </StrictMode>,
 );
